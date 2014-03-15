4a00d7d23db7655da5ff16f3351f4808294d6e45
Alberto Buffolino <a.buffolino@gmail.com>
2014-03-15 14:52:46 +0100

    Introduced a bool switch to include & symbol when necessary

--- addon/globalPlugins/rapidSettings/msg.py
+++ addon/globalPlugins/rapidSettings/msg.py
@@ -2,7 +2,13 @@
 # A simple module to bypass the addon translation system,
 # so it can take advantage from the NVDA translations directly.
 
-def message(message):
+def message(message, amp=False):
+	"""Return translated message according to NVDA local translations, put amp to True to not remove & symbol"""
 	if message == "":
+		# blank string translated reports NVDA information, so...
 		return message
-	return _(message).replace('&', '')
+	message = _(message)
+	# & is useful for menu, but generally we don't want it
+	if not amp:
+		message = message.replace('&', '')
+	return message
